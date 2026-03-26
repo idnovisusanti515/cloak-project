97e6f171a6f4b74554412218c74297610ceadfd3
idnovisusanti515 <idnovisusanti588@gmail.com>
2026-03-26 14:32:02 +0700

    Update route.js

--- api/route.js
+++ api/route.js
@@ -26,3 +26,9 @@ export default async function handler(req, res) {
 
   return res.writeHead(404).end();
 }
+// HANDLE SLUG
+if (url.pathname === "/pusat4d-telah-melakukan-wd-1112") {
+  return res.writeHead(302, {
+    Location: "https://dub.sh/XXXX" // link Dub lo
+  }).end();
+}
